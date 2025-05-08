19f23526046b5be69d658f3fea95d9b7c39490a0
Chaitanya Pamarthi <pamarthichaitanya1@gmail.com>
2025-05-08 08:05:36 +0530

    first commit

--- BasicJava/Day3/AdamNumber.java
+++ BasicJava/Day3/AdamNumber.java
@@ -3,36 +3,28 @@ package BasicJava.Day3;
 import java.util.Scanner;
 
 public class AdamNumber {
-
-    static int reverseNumber(int num) {
-        int reversed = 0;
-        while (num > 0) {
-            int digit = num % 10;
-            reversed = reversed * 10 + digit;
-            num /= 10;
-        }
-        return reversed;
-    }
-
-    static boolean isAdamNumber(int number) {
-        int square = number * number;
-        int reversedNumber = reverseNumber(number);
-        int reversedSquare = reversedNumber * reversedNumber;
-        int reversedSquareOfSquare = reverseNumber(square);
-
-        return reversedSquareOfSquare == (reversedSquare);
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        scanner.close();
-
-        if (isAdamNumber(number)) {
-            System.out.println(number + " is an Adam number.");
+        int n = sc.nextInt();
+        int sq = n * n;
+        int rev = 0;
+        while (n != 0) {
+            int r = n % 10;
+            rev = (rev * 10 + r);
+            n = n / 10; 
+        }
+        int revsq = rev * rev;
+        int revsqrev = 0;
+        while (revsq != 0) {
+            int r = revsq % 10;
+            revsqrev = (revsqrev * 10 + r);
+            revsq = revsq / 10; 
+        }
+        if (sq == revsqrev) {
+            System.out.println("Adam Number");
         } else {
-            System.out.println(number + " is not an Adam number.");
+            System.out.println("Not an Adam Number");
         }
     }
 }
--- BasicJava/Day3/KaprekarNumber.java
+++ BasicJava/Day3/KaprekarNumber.java
@@ -3,26 +3,14 @@ package BasicJava.Day3;
 import java.util.Scanner;
 
 public class KaprekarNumber {
-    public static boolean isKaprekar(int num) {
-        int square = num * num;
-        String strNum = String.valueOf(square);
-        int len = strNum.length();
-        int rightPart = Integer.parseInt(strNum.substring(len - String.valueOf(num).length()));
-        int leftPart = (len > String.valueOf(num).length()) ? Integer.parseInt(strNum.substring(0, len - String.valueOf(num).length())) : 0;
-
-        return (leftPart + rightPart == num);
-    }
-
-    public static void main(String[] args) {
+        public static void main(String[] args) 
+        {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
         scanner.close();
-
-        if (isKaprekar(num)) {
-            System.out.println("Kaprekar Number");
-        } else {
-            System.out.println("Not a Kaprekar Number");
         }
-    }
+
+        
+    
 }
