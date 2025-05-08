ba5a74df164126b63a8d094420ef5869ea120ca3
Chaitanya Pamarthi <pamarthichaitanya1@gmail.com>
2025-05-08 10:19:35 +0530

    first commit

--- BasicJava/Day4/Pattren.java
+++ BasicJava/Day4/Pattren.java
@@ -1,5 +1,17 @@
 package BasicJava.Day4;
 
 public class Pattren {
-    public staic 
+    public staic void main(String[] args);{
+        Scanner sc = new sccanner(System.in);
+        int n ;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int sum = 1;
+        for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j--)   
+                    System.out.print(sum);
+                    sum++; 
+            }    
+    }
 }
