67a84db73550bec418ea94f4de56666152759995
Chaitanya Pamarthi <pamarthichaitanya1@gmail.com>
2025-05-09 09:48:50 +0530

    first commit

---
+++ BasicJava/Day5/insectAllNumbers.java
@@ -0,0 +1,13 @@
+package BasicJava.Day5;
+
+import java.util.Scanner;
+
+public class insectAllNumbers {
+    public static void maibn(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[] = new int[n];
+        arr[n] = sc.nextInt();
+        System.out.print(arr[n]+" ");
+        } 
+  } 
