488d861f1d1b7c31a38f5c5428c24e9c2100a676
Chaitanya Pamarthi <pamarthichaitanya1@gmail.com>
2025-05-09 08:09:00 +0530

    first commit

--- BasicJava/Day4/Pattren.java
+++ BasicJava/Day4/Pattren.java
@@ -5,9 +5,9 @@ public class Pattren {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 0; i <= n-1; i++)
+        for(int i = 1; i <= n; i++)
             {
-                for(int j = i; j <= n; j++)
+                for(int j = 1; j <= i; j++)
                     {
                         System.out.print(" * ");
                     }
