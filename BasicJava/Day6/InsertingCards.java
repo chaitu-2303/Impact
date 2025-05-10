f4134989bd765c4eaa1eb35261a02da4275b5287
Chaitanya Pamarthi <pamarthichaitanya1@gmail.com>
2025-05-10 09:41:29 +0530

    fourth commit

--- BasicJava/Day6/InsertingCards.java
+++ BasicJava/Day6/InsertingCards.java
@@ -7,8 +7,22 @@ public class InsertingCards
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for(int i = 0; i < size; i++){
+            arr[i] = sc.nextInt();
+        }
+        int pos = sc.nextInt();
+        if (pos > size) {
+            System.out.println("Invalid position");
+        }else{
+            int element = sc.nextInt();
+            for(int i = 0; i < size; i++){
+                if(i == pos - 1){
+                    System.out.println(element);
+                }
+                System.out.println(arr[i]);
+            }
+        }    
     }
 }
