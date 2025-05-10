18ddbe2e0dfa4a1c281b7f958378bfe11711e470
Chaitanya Pamarthi <pamarthichaitanya1@gmail.com>
2025-05-10 09:22:01 +0530

    third  commit

---
+++ BasicJava/Day6/InsertingCards.java
@@ -0,0 +1,14 @@
+package BasicJava.Day6;
+
+import java.util.Scanner;
+
+public class InsertingCards 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+    }
+}
