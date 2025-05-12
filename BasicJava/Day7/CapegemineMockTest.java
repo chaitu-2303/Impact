7c05de9657fc19009199a4ab0645997c0ba44690
Chaitanya Pamarthi <pamarthichaitanya1@gmail.com>
2025-05-12 09:53:08 +0530

    second commit

---
+++ BasicJava/Day7/CapegemineMockTest.java
@@ -0,0 +1,10 @@
+package BasicJava.Day7;
+
+import java.util.Scanner;
+
+public class CapegemineMockTest {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+}
+}
