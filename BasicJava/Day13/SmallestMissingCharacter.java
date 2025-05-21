c3970b50a6f41b3f3aa3eedca9492ef161db20d3
Chaitanya Pamarthi <pamarthichaitanya1@gmail.com>
2025-05-21 08:54:51 +0530

    first commit

---
+++ BasicJava/Day13/SmallestMissingCharacter.java
@@ -0,0 +1,9 @@
+package BasicJava.Day13;
+
+import java.util.Scanner;
+
+public class SmallestMissingCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
