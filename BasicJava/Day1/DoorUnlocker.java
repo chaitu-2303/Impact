210e722be8f2213fe34c0954551bc7039ad53a22
Chaitanya Pamarthi <pamarthichaitanya1@gmail.com>
2025-05-24 07:49:41 +0530

    first commit

--- BasicJava/Day1/DoorUnlocker.java
+++ BasicJava/Day1/DoorUnlocker.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class DoorUnlocker {
     public static void main(String[] args) 
     {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); 
-        sc.close();
-        
-        int first_door = n/1000; 
-        int second_door = n%10;
-        System.out.println(first_door+ second_door); 
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt(); 
+            
+            int first_door = n/1000; 
+            int second_door = n%10;
+            System.out.println(first_door + second_door); 
+        }
     }
 }
---
+++ BasicJava/Day16/Eachcolsum.java
@@ -0,0 +1,27 @@
+package BasicJava.Day16;
+
+import java.util.*;
+
+public class Eachcolsum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns:  ");
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int[][] arr = new int[rows][cols];
+        System.out.println("Enter the elements of the array : ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The sum of each row is:");
+        for (int i = 0; i < rows; i++) {
+            int sum = 0;
+            for (int j = 0; j < cols; j++) {
+                sum += arr[i][j];
+            }
+            System.out.println("Row " + (i + 1) + ": " + sum);
+        }
+    }
+}
